c560797a4587174c656e0d1b940da00b7bf67a40
Felipe 0liveira <felpd3v@gmail.com>
2019-03-09 14:58:48 -0300

    Get main subject of the trend

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 const webdriver = require("selenium-webdriver");
 const chrome = require('selenium-webdriver/chrome');
 const path = require('chromedriver').path;
-
-// Chrome Serive
-const service = new chrome.ServiceBuilder(path).build();
-chrome.setDefaultService(service);
+const chalk = require('chalk');
 
 
-(() => {
+(async () => {
+    // Chrome Serive
+    const service = new chrome.ServiceBuilder(path).build();
+    chrome.setDefaultService(service);
+
     // Chrome Driver
-    const driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).setChromeOptions(new chrome.Options().headless()).build()
+    const driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build(); // .setChromeOptions(new chrome.Options().headless())
     const By = webdriver.By;
+    const until = webdriver.until;
 
-    driver.get('https://trends.google.com.br/trends/?geo=BR');
-    console.log('Trends ON');
+    try {
 
-    const lastTrend = driver.findElement(By.css('editorial-item.editorial-not-main-card .editorial-item-text.title'));
+        driver.get('https://trends.google.com.br/trends/?geo=BR');
+        console.log(`Getting ${chalk.blue('Google Trends')}.\n`);
 
-    lastTrend.getText().then(text => {
-        console.log('Assunto mais falado no Brasil:', text);
-        lastTrend.click();
-    });
+        const subject = {};
 
+        // Subject Title
+        const trend = driver.findElement(By.css('.editorials-content-container editorial-item .editorial-item-container .editorial-item .editorial-item-text.main-card-title'));
+        await trend.getText().then(trendTitle => {
+            console.log(`=> Trending Topic in Brazil:`, chalk.black.bgYellow(trendTitle));
+            subject.title = trendTitle;
+        });
+
+        // Goto Subject's Page
+        await trend.click();
+
+        // Subject About
+        const aboutTrend = driver.findElement(By.tagName('pre'));
+        await driver.wait(until.elementIsVisible(aboutTrend), 5000);
+        await aboutTrend.getText().then(trendAbout => {
+            console.log(`=> About ${subject.title}:`, chalk.black.bgYellow(trendAbout));
+            subject.about = trendAbout;
+        });
+
+    } catch (err) {
+        console.log(chalk.red('Error:'), err.name);
+        console.log(err);        
+        
+        console.log(chalk.cyan('Closing Google Chrome...'));
+        driver.close();
+    }
 })();
